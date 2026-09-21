518462252af65744dd3ab0cc3e8f27d1defb3565
archivenovia98-cell <archivenovia98@gmail.com>
2026-09-21 22:33:05 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,13 @@
+const CACHE='lucky-draw-v4';
+const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));});
+self.addEventListener('activate',e=>{e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))]));});
+self.addEventListener('fetch',e=>{
+  if(e.request.method!=='GET') return;
+  e.respondWith(caches.match(e.request).then(cached=>{
+    const network=fetch(e.request).then(resp=>{
+      const copy=resp.clone(); caches.open(CACHE).then(c=>c.put(e.request,copy)); return resp;
+    }).catch(()=>cached||caches.match('./index.html'));
+    return cached||network;
+  }));
+});
